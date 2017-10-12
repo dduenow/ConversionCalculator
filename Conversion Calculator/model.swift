4a3a6eb54ca58516e02bd237cc0574c560af3265
Sonu Chavakula <sonuchavakula@Sonus-MacBook-Pro.local>
2017-10-12 02:23:24 -0500

    added first version of the functions

--- Conversion Calculator/model.swift
+++ Conversion Calculator/model.swift
@@ -3,7 +3,32 @@
 //  Conversion Calculator
 //
 //  Created by Sonu Chavakula on 10/3/17.
+//  Owned by Sonu Chavakula
 //  Copyright © 2017 David Duenow. All rights reserved.
 //
 
 import Foundation
+
+class Conversions {
+    
+    func ftoC(tempInF:Double) -> Double {
+        let celsius = (tempInF - 32.0) * (5.0/9.0)
+        return celsius as Double
+    }
+    
+    func milestoKilometers(speedInMPH:Double) -> Double {
+        let speedInKPH = speedInMPH * 1.60934
+        return speedInKPH as Double
+    }
+    
+    func inchesToCentimeters(depthInInches:Double) -> Double {
+        let depthInCentimeters = depthInInches * 2.54
+        return depthInCentimeters as Double
+    }
+    
+    func centimetersToInches(depthInCentimeters:Double) -> Double {
+        let depthInInches = depthInCentimeters / 2.54
+        return depthInInches as Double
+    }
+
+}
